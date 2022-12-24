71f996a66fd1acdb333d7a3bf5053bdbeacb4517
Isabirye Simon Peter <kaswadaniel100@gmail.com>
2022-12-24 08:51:09 +0300

    multhreading example1

--- Multitasking/src/appdevelopment/threading.java
+++ Multitasking/src/appdevelopment/threading.java
@@ -1,5 +1,35 @@
 package appdevelopment;
 
-public class threading {
+public class threading extends Thread {
+	//first program under threading
+	@Override
+	public void run() {
+		for(int i=0;i<60;i++) {
+			//this is counting seconds that count a minute
+		System.out.println("Seconds:"+i);
+		//this makes the figures sleep for some time before changing
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		
+		}
+		for(int j=0;j<60;j++) {
+			//changing the minutes
+			System.out.println("Minutes:"+(j+1));
+			try {
+				Thread.sleep(60000);
+			} catch (InterruptedException e) {
+			}
+		}
+		
+	}
+	public static void main(String[] args) {
+		threading thread=new threading();
+		threading thread2=new threading();
+		
+		thread.start();
+		thread2.run();
+	}
 
 }
